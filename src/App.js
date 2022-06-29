b883411f22f642d638c410da4a8034c19c02c2bc
Arzoo-tech <arzoos.official@gmail.com>
2022-06-29 20:34:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+//this hook helps us to use redux store state 
+//in any component
+import {useSelector} from 'react-redux';
+//importing dispatch to pass actions to reducers
+import {useDispatch} from 'react-redux';
+//importing actions
+import {increment,decrement} from './actions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const counter =useSelector(state=> state.counter);
+  const isLogged =useSelector(state=> state.isLogged);
+  const dispatchEvent=useDispatch();
+  //we can als do:
+  //const counter =useSelector(state=> state)
+  return( 
+  <div className="App">
+  <h1>Testing redux</h1>
+  <h2>Counter {counter}</h2>
+  <button onClick={()=>dispatchEvent(increment())}>+</button>
+  <button  onClick={()=>dispatchEvent(decrement())}>-</button>
+  {isLogged?<h2>Info that I shouldn't see</h2>:''}
+  </div>
+  )
 }
 
 export default App;
